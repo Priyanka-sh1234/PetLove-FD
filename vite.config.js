952cf520dea42fa2ad0575bb432512a01b50468a
Priyanka-sh1234 <pankusharma3030@gmail.com>
2025-05-23 16:36:01 +0530

    Host removal

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,4 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss()],
-
-    server: {
-    host: '0.0.0.0',
-    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
-    allowedHosts: ['petlove-fd.onrender.com']
-
-  }
 })
