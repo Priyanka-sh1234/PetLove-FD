01088f5f8ce945edc84052831349b3b8cbb2899b
Priyanka-sh1234 <pankusharma3030@gmail.com>
2025-05-23 15:55:09 +0530

    Host Addition

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     server: {
     host: '0.0.0.0',
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
-    allowedHosts: ['trendify-frontend-22gf.onrender.com']
+    allowedHosts: ['petlove-fd.onrender.com']
 
   }
 })
