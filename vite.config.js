dee3d835eb089ab1a9f93c55ee46da6be6866676
Priyanka-sh1234 <pankusharma3030@gmail.com>
2025-05-23 15:19:40 +0530

    Server in Vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss()],
+
+    server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    allowedHosts: ['trendify-frontend-22gf.onrender.com']
+
+  }
 })
