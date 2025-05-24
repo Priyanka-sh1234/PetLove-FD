19972121fd28837059bd47ad92120a836ff240f9
Priyanka-sh1234 <pankusharma3030@gmail.com>
2025-05-24 17:40:39 +0530

    instance timeout updation

--- axios.instance.js
+++ axios.instance.js
@@ -1,49 +1,44 @@
-import axios from 'axios'
+import axios from 'axios';
 import { baseURL } from './config';
 
-const instance = axios.create(
-    {
-        baseURL,
-        timeout:1000,
-    }
-);
+const instance = axios.create({
+  baseURL,
+  timeout: 10000, // Increased timeout
+});
 
+// Request Interceptor
 instance.interceptors.request.use(
-    async(config)=>{
-        try{
-            const accessToken= localStorage.getItem("Token");
-            config.headers.authorization= `Bearer ${accessToken}`;
-            return config;
-        }
-        catch(error){
-            console.error("error setting authoriation header", error);
-            return Promise.reject(error);
-        }
-    },
-
-    (error)=>{
-        console.error("request error:", error);
-        return Promise.reject(error);
+  async (config) => {
+    try {
+      const accessToken = localStorage.getItem("Token");
+      if (accessToken) {
+        config.headers.authorization = `Bearer ${accessToken}`;
+      }
+      return config;
+    } catch (error) {
+      console.error("Error setting authorization header", error);
+      return Promise.reject(error);
     }
-
-    
+  },
+  (error) => {
+    console.error("Request error:", error);
+    return Promise.reject(error);
+  }
 );
 
-//Response Interceptor
+// Response Interceptor
 instance.interceptors.response.use(
-    (response)=>{
-        // console.log("Response Data:",  response.data)
-        console.log("Instance Called")
-        // localStorage.setItem("ID",response.data.data._id)
-        return response;
-    },
-    (error)=>{
-        console.error("Response Error", error);
-        if(error.response.status===401){
-            console.log("unauthorized error, redirecing to login...")
-        }
-        return Promise.reject(error);
+  (response) => {
+    console.log("Instance called");
+    return response;
+  },
+  (error) => {
+    console.error("Response Error", error.toJSON?.() || error);
+    if (error.response?.status === 401) {
+      console.log("Unauthorized error, redirecting to login...");
     }
+    return Promise.reject(error);
+  }
 );
 
 export default instance;
