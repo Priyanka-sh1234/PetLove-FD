bd4a8ea3a83d68221ff0d0081a447a61c3b4af61
Priyanka-sh1234 <pankusharma3030@gmail.com>
2025-05-24 16:59:46 +0530

    axios updation

--- axios.instance.js
+++ axios.instance.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create(
     {
-        baseURL:'http://localhost:4000',
+        baseURL:'https://petlove-bd.onrender.com',
         timeout:1000,
     }
 );
